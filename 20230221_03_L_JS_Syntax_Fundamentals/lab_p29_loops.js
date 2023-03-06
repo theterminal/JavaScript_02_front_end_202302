74fdc39ea026d3445bf5603778ea50c7e0630f54
theterminal <kirilk777@gmail.com>
2023-03-06 17:31:52 +0200

    initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p29_loops.js
@@ -0,0 +1,24 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Loops
+
+
+// ____________ For Loop ______________
+// Repeats until the condition is evaluated.
+// Incrementation in the condition.
+
+
+for (let i = 1; i <= 5; i++){
+    console.log(i);
+  }
+
+
+// ____________ While Loop ______________
+// Does the same, but has different structure
+// Incrementation outside the condition
+
+
+let j = 1
+while (j <= 5) {
+  console.log(j);
+  j++;
+}
