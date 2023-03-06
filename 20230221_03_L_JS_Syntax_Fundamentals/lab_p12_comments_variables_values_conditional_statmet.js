9eb6e2e1e2d25524b1593d2ddc6c747666833385
theterminal <kirilk777@gmail.com>
2023-03-06 14:37:12 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p12_comments_variables_values_conditional_statmet.js
@@ -0,0 +1,26 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// JavaScript Syntax
+
+
+// _________________ Comments __________________________________________________________________
+
+
+// This is a single line comment
+/* This is a multiline comment */
+
+
+// _________________ Variables, Declairing a variable, Conditional Statement ___________________
+
+
+let a = 5;
+let b = 10;
+
+if (b > a) {
+    console.log(b);
+}
+
+// In the example above 'let' is used to declair a variable
+// The name of the variables are 'a' and 'b'
+// '5' and '10' are the values of the varialbles
+
+// Everything between the curly brackets is the 'body of the conditional statement', in this case 'console.log(b)'
