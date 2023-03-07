f48219398283ef792f9132d46bfb383e3b98a1c0
theterminal <kirilk777@gmail.com>
2023-03-07 14:09:38 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p42_array_methods_unshift.js
@@ -0,0 +1,17 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Array Methods - Pop, Push, Shift, Unshift, Splice, Reverse, Join, Slice, Includes, IndexOf, ForEach, Mapm, Find, Filter
+
+
+// __________ unshift() ___________
+
+
+// The unshift() method adds one or more elements to the beginning of an array and
+// returns the new length of the array.
+
+
+let nums = [40, 50, 60];
+
+console.log(nums.length);                       // 3
+console.log(nums.unshift(30));                  // 4 (nums.length)
+console.log(nums.unshift(10,20));               // 6 (nums.length)
+console.log(nums);                              // [ 10, 20, 30, 40, 50, 60 ]
