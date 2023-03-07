a3e4a82d314c6fb605886602a73f4bc731f312c0
theterminal <kirilk777@gmail.com>
2023-03-07 14:09:53 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p43_array_methods_splice.js
@@ -0,0 +1,20 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Array Methods - Pop, Push, Shift, Unshift, Splice, Reverse, Join, Slice, Includes, IndexOf, ForEach, Mapm, Find, Filter
+
+
+// __________ splice() ___________
+
+
+// Changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+
+
+let nums = [1, 3, 4, 5, 6];
+
+nums.splice(1, 0, 2);                       // inserts at index 1
+console.log(nums);                          // [ 1, 2, 3, 4, 5, 6 ]
+nums.splice(4, 1, 19);                      // replaces 1 element at index 4
+console.log(nums);                          // [ 1, 2, 3, 4, 19, 6 ]
+
+let el = nums.splice(2, 1);                 // removes 1 element at index 2
+console.log(nums);                          // [ 1, 2, 4, 19, 6 ]
+console.log(el);                            // [ 3 ]
