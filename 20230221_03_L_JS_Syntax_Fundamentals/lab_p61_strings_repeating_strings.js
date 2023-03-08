68a154eaccce016983ef1b287c75110791ae5715
theterminal <kirilk777@gmail.com>
2023-03-08 17:29:58 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p61_strings_repeating_strings.js
@@ -0,0 +1,19 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Manipulating Strings
+// Repeating Strings
+
+
+// ___________ repeat(count) - Creates a new string repeated count times ____________
+
+let n = 7;
+for(let i = 1; i <= n; i++) {
+  console.log('*'.repeat(i));
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
