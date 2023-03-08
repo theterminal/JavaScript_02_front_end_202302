c4d6871ffbc4c929ec714bce29d1c287653a3701
theterminal <kirilk777@gmail.com>
2023-03-08 17:30:14 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p65_strings_startsWith_endsWith.js
@@ -0,0 +1,14 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Manipulating Strings
+// startsWith / endsWith
+
+
+// Use startsWith() to determine whether a string begins with the characters of a specified substring
+
+let text_1 = "My name is John";
+console.log(text_1.startsWith('My'));                     // Expected output: true
+
+
+// Use endsWith() to determine whether a string ends with thecharacters of a specified substring
+let text_2 = "My name is John";
+console.log(text_2.endsWith('John'));                     // Expected output: true
