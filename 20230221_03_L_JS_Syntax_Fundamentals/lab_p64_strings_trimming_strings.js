a73032c7e15be9a259d165e275d1d012bf065448
theterminal <kirilk777@gmail.com>
2023-03-10 11:34:33 +0200

    Syntax corrected

--- 20230221_03_L_JS_Syntax_Fundamentals/lab_p64_strings_trimming_strings.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p64_strings_trimming_strings.js
@@ -3,13 +3,13 @@
 // Trimming Strings
 
 
-// Use trim() method to remove whitespaces (spaces, tabs, no-break space, etc. ) from both ends of a string
+// Use trim() method to remove white spaces (spaces, tabs, no-break space, etc. ) from both ends of a string
 
 let text_1 = "   Annoying spaces       ";
 console.log(text_1.trim());                                 // Output: "Annoying spaces"
 
 
-// Use trimStart() or trimEnd() to remove whitespaces only at the beginning or at the end
+// Use trimStart() or trimEnd() to remove white spaces only at the beginning or at the end
 
 let text_2 = "   Annoying spaces       ";
 text_2 = text_2.trimStart();
