09533f48defbddc9a030833f8034c43713bd666e
theterminal <kirilk777@gmail.com>
2023-03-10 11:33:33 +0200

    Syntax corrected

--- 20230221_03_L_JS_Syntax_Fundamentals/lab_p12_comments_variables_values_conditional_statmet.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/lab_p12_comments_variables_values_conditional_statmet.js
@@ -9,7 +9,7 @@
 /* This is a multiline comment */
 
 
-// _________________ Variables, Declairing a variable, Conditional Statement ___________________
+// _________________ Variables, Declaring a variable, Conditional Statement ___________________
 
 
 let a = 5;
@@ -19,8 +19,8 @@ if (b > a) {
     console.log(b);
 }
 
-// In the example above 'let' is used to declair a variable
+// In the example above 'let' is used to declare a variable
 // The name of the variables are 'a' and 'b'
-// '5' and '10' are the values of the varialbles
+// '5' and '10' are the values of the variables
 
 // Everything between the curly brackets is the 'body of the conditional statement', in this case 'console.log(b)'
