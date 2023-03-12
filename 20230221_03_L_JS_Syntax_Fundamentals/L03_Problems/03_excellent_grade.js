809a146e8cc20876e74b8042fc7f6f6d0cb2d022
theterminal <kirilk777@gmail.com>
2023-03-12 08:15:49 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/03_excellent_grade.js
@@ -0,0 +1,15 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 03 - Excellent Grade - judge: https://judge.softuni.org/Contests/Practice/Index/3785#2
+
+
+function solve(grade) {
+    if (grade >= 5.50) {
+        console.log('Excellent');
+    } else {
+        console.log('Not excellent');
+    }
+}
+
+
+solve(5.50);
+solve(4.35);
