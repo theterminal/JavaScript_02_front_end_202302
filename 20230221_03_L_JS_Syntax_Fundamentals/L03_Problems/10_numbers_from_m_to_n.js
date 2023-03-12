36f1feb962c9a4921e18677ee0024564361be487
theterminal <kirilk777@gmail.com>
2023-03-12 15:15:08 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/10_numbers_from_m_to_n.js
@@ -0,0 +1,12 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 10 - Numbers from M to N - judge: https://judge.softuni.org/Contests/Practice/Index/3785#9
+
+
+function numbersFromMToN(m, n) {
+    for (let i = m; i >= n; i--) {
+        console.log(i);       
+    }
+}
+
+numbersFromMToN(6, 2);
+numbersFromMToN(4, 1);
