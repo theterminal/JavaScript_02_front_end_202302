0c988851988c63cd1b9fe6980c068136d4076268
theterminal <kirilk777@gmail.com>
2023-03-12 15:07:35 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/09_numbers_from_1_to_5.js
@@ -0,0 +1,11 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 09 - Numbers from 1 to 5 - judge: https://judge.softuni.org/Contests/Practice/Index/3785#8
+
+
+function numbersFrom1To5() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);       
+    }
+}
+
+numbersFrom1To5();
