2b7d7c5849b0641fed68feb0e1e9882a3d301001
theterminal <kirilk777@gmail.com>
2023-03-12 13:13:21 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/05_math_operations.js
@@ -0,0 +1,20 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 05 - Math Operations - judge: https://judge.softuni.org/Contests/Practice/Index/3785#4
+
+
+function mathOperations(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+    console.log(result);
+}
+
+
+mathOperations(5, 6, '+');
+mathOperations(3, 5.5, '*');
