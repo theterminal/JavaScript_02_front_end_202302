d5f23e734b8b553c062b446090a23e4250f9d07f
theterminal <kirilk777@gmail.com>
2023-03-12 07:54:50 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/01_ multiply_the_number_by_2.js
@@ -0,0 +1,13 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 01 - Multiply the number by 2 - judge: https://judge.softuni.org/Contests/Practice/Index/3785#0
+
+
+function solve(number) {
+    result = number * 2;
+    console.log(result);
+}
+
+
+solve(2);
+solve(5);
+solve(20);
