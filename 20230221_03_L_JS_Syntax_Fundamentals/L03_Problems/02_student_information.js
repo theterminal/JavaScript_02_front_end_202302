38c542d6db3caaa3493f6ce69e648db0398c547f
theterminal <kirilk777@gmail.com>
2023-03-12 08:07:42 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/02_student_information.js
@@ -0,0 +1,12 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Problem 02 - Student Information - judge: https://judge.softuni.org/Contests/Practice/Index/3785#1
+
+
+function solve(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+
+solve('John', 15, 5.54678);
+solve('Steve', 16, 2.1426);
+solve('Marry', 12, 6.00);
