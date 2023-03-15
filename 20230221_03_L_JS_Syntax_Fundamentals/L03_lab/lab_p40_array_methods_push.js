dfba1c1978717dc9846391994da1bfe9c3c6ccfd
theterminal <kirilk777@gmail.com>
2023-03-15 07:31:23 +0200

    More content added

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p40_array_methods_push.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p40_array_methods_push.js
@@ -5,8 +5,7 @@
 // __________ push() ___________
 
 
-// The push() method adds one or more elements to the end of an array and
-// returns the new length of the array.
+// The push() method adds one or more elements to the end of an array and returns the new array length.
 
 
 let nums = [10, 20, 30, 40, 50, 60, 70];
@@ -15,4 +14,6 @@ console.log(nums.length);                                                   // 7
 console.log(nums.push(80));                                                 // 8 (nums.length)
 console.log(nums);                                                          // [ 10, 20, 30, 40, 50, 60, 70, 80 ]
 
+nums.push(...[6, 7, 8]);                                                    // rest operator can be used
+console.log(nums);
 
