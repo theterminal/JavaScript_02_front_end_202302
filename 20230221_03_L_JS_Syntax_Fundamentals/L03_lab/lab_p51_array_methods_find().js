a0ea55046b0cfa7fc63c08f228e7c21e09b1e623
theterminal <kirilk777@gmail.com>
2023-03-15 07:32:25 +0200

    Added content

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p51_array_methods_find().js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p51_array_methods_find().js
@@ -5,7 +5,7 @@
 // __________ find() ___________
 
 
-// Returns the first found value in the array, if an element in the array satisfies the provided testing function or undefined if not found
+// Returns the first found value in the array, if an element in the array satisfies the provided testing function or undefined if not found.
 
 
 let array_1 = [5, 12, 8, 130, 44];
@@ -15,3 +15,19 @@ let found = array_1.find(function(element) {
 })
 
 console.log(found);                             // 12
+
+
+// Another shorter syntax
+
+let array_2 = [-12, -15.75, 0, 0.125, 1, 15, 100, 125, 125.75, 10000];
+let found_2 = array_2.find((num) => num > 100);
+
+console.log(found_2);
+
+
+// Another syntax
+
+let array_3 = [-12, -15.75, 0, 0.125, 1, 15, 100, 125, 125.75, 10_000];
+let found_3 = array_3.find((num) => num > 125 && num < 10_000);
+
+console.log(found_3);
