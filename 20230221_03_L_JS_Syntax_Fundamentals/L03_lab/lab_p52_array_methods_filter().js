d06754d844f47f93904e6d3c55634f71cfa6cb6c
theterminal <kirilk777@gmail.com>
2023-03-15 07:32:28 +0200

    Added content

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p52_array_methods_filter().js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p52_array_methods_filter().js
@@ -10,12 +10,44 @@
 // Does not mutate the array on which it is called
 
 
-let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];              // Filter array items based on search criteria (query)
+console.log('\n__________ Example 1 ___________\n');
+
+
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];                   // Filter array items based on search criteria (query)
 
 function filterItems(arr, query) {
     return arr.filter(function(el) {
         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-  }
+  })
 }
 
-console.log(filterItems(fruits, 'ap'));                                   // ['apple', 'grapes']
+console.log(filterItems(fruits, 'ap'));                                          // ['apple', 'grapes']
+
+
+console.log('\n___________ Example 2 ____________\n');
+
+
+let numbers = [12, 22, 43];
+let result = numbers.filter((x) => x % 2 === 0);
+
+console.log(result);
+
+
+console.log('\n___________ Example 3 ___________ another syntax ____________\n');
+
+
+let nums = [12, 22, 43, 62, 102];
+let evenNums = nums
+  .filter((x) => x % 2 === 0);
+
+console.log(evenNums);
+console.table(evenNums);
+
+
+console.log('\n____________ Example 4 ____________\n');
+
+
+let words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+let result_4 = words.filter(word => word.length > 6);
+
+console.log(result_4);                                                          // [ 'exuberant', 'destruction', 'present' ]
