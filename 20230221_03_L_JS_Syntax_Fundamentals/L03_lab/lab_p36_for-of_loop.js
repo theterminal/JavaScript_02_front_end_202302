18f19fb3a78a482d39c07080b24fda49172a959d
theterminal <kirilk777@gmail.com>
2023-03-15 07:31:10 +0200

    Additional content

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p36_for-of_loop.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p36_for-of_loop.js
@@ -4,6 +4,12 @@
 
 // Iterates through all elements in a collection.
 // Cannot access the current index.
+// Usually it is used for 'read only' operations like go through all the elements and print them out...
+// If you need to change the elements use the regular 'for' loop.
+
+
+console.log(`\n_________ Example 1 _________\n`);
+
 
 let collection = [10, 20, 30];
 
@@ -14,12 +20,25 @@ for (let el of collection) {
     }
 
 
-// _____________ Print an Array with For-of _____________
+console.log(`\n_________ Example 2 _________ Print an Array with For-of __________\n`);
 
 
 let numbers = [1, 2, 3, 4, 5];
-let output = '';
 
 for (let number of numbers)
-    output += `${number} `;
-console.log(output);                                    // 1 2 3 4 5 
+    console.log(number);                                    // 1
+                                                            // 2
+                                                            // 3
+                                                            // 4
+                                                            // 5
+
+
+console.log(`\n_________ Example 3 _________ Print an Array with For-of __________\n`);
+
+
+let numbers2 = [1, 2, 3, 4, 5];
+let output2 = '';
+
+for (let number of numbers2)
+    output2 += `${number} `;
+console.log(output2);                                    // 1 2 3 4 5 
