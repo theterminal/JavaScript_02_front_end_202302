d67cca17b77b11de4107c1109c3ec9a65df7a6a4
theterminal <kirilk777@gmail.com>
2023-03-17 07:52:21 +0200

    Initial

---
+++ 20230223_04_E_JS_Syntax_Fundamentals/07_chars_to_string.js
@@ -0,0 +1,13 @@
+// 20230223 - JavaScript Front-End 202302 - JS Syntax Fundamentals - Exercise
+// Problem 07 - Chars to String - judge: https://judge.softuni.org/Contests/Compete/Index/3786#6
+
+
+function charsToString(par_1, par_2, par_3) {
+    let result = par_1 + par_2 + par_3;
+    console.log(result);
+}
+
+
+charsToString('a', 'b', 'c');                               // abc
+charsToString('%', '2', 'o');                               // %2o
+charsToString('1', '5', 'p');                               // 15p
