a2203c8175bb1007c57f99a4d8d3c8b4893a2a40
theterminal <kirilk777@gmail.com>
2023-03-22 12:41:50 +0200

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_03_array_sort()_reverse().js
@@ -0,0 +1,23 @@
+// 20230221 - JavaScript Front-End 202302 - JS Syntax Fundamentals
+// Array method sort(), reverse()
+// https://www.w3schools.com/js/js_array_sort.asp
+
+
+console.log('\n ____________ sort() _____________ \n');
+// The sort() method sorts the array alphabetically.
+
+
+arr = ['Jerry', 'Bob', 'Apple'];
+arr.sort();
+
+console.log(arr);                                       // [ 'Apple', 'Bob', 'Jerry' ]
+
+
+console.log('\n ____________ reverse() _____________ \n');
+// The reverse() method orders the array in reverse.
+
+
+arr2 = ['Jerry', 'Bob', 'Apple'];
+arr2.reverse();
+
+console.log(arr2);                                      // [ 'Apple', 'Bob', 'Jerry' ]
