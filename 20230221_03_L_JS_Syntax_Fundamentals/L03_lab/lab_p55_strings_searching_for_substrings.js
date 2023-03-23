248542ded221186e56c9dfa010830a2571a9a00b
theterminal <kirilk777@gmail.com>
2023-03-23 19:09:09 +0200

    Refactoring

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p55_strings_searching_for_substrings.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p55_strings_searching_for_substrings.js
@@ -5,6 +5,7 @@
 
 // ___________ indexOf(substr) ____________
 
+
 let str_1 = "I am JavaScript developer";
 
 console.log(str_1.indexOf("Java"));               // Expected output: 5               
@@ -13,7 +14,8 @@ console.log(str_1.indexOf("java"));               // Expected output: -1
 
 // ___________ lastIndexOf(substr) _____________
 
+
 let str_2 = "Intro to programming";
 let last = str_2.lastIndexOf("o");
 
-console.log(last);                              // Expected output: 11
+console.log(last);                                // Expected output: 11
