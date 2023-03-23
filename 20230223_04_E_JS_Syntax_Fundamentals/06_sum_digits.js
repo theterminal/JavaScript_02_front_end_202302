0f6f5a66b60ec4b8dee9adef377ab8f67b87b050
theterminal <kirilk777@gmail.com>
2023-03-23 07:37:01 +0200

    Refactored

--- 20230223_04_E_JS_Syntax_Fundamentals/06_sum_digits.js
+++ 20230223_04_E_JS_Syntax_Fundamentals/06_sum_digits.js
@@ -2,6 +2,9 @@
 // Problem 06 - Sum Digits - judge: https://judge.softuni.org/Contests/Compete/Index/3786#5
 
 
+// ____________ version 1 ____________ converting to string ____________
+
+
 function sumDigits(number) {
     let result = 0;
     number = String(number);
@@ -16,3 +19,25 @@ function sumDigits(number) {
 sumDigits(245678);                                  // 32
 sumDigits(97561);                                   // 28
 sumDigits(543);                                     // 12
+
+
+// ____________ version 2 ____________ mathematical version ____________
+
+
+function sumDigits2(num) {
+    let sum = 0;
+
+    while (num > 0) {
+        let lastDigit = num % 10;
+        sum += lastDigit;
+        num = Math.trunc(num / 10);                 // truncates everything after the decimal point
+        // num = Math.floor(num / 10);
+    }
+
+    console.log(sum);
+}
+
+
+sumDigits2(245678);                                 // 32
+sumDigits2(97561);                                  // 28
+sumDigits2(543);                                    // 12
