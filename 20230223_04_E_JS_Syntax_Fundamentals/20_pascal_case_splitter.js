cf72cf43fbf378b1a71dffb423167a7ebf35790e
theterminal <kirilk777@gmail.com>
2023-03-24 12:28:13 +0200

    Content added

--- 20230223_04_E_JS_Syntax_Fundamentals/20_pascal_case_splitter.js
+++ 20230223_04_E_JS_Syntax_Fundamentals/20_pascal_case_splitter.js
@@ -2,6 +2,22 @@
 // Problem 20 - Pascal Case Splitter - judge: https://judge.softuni.org/Contests/Compete/Index/3786#19
 
 
+// _________________ version 2 ____________________
+
+
+function pascalCaseSplitter2(string) {
+    console.log(string
+        .split(/(?=[A-Z])/)
+        .join()(", "));
+}
+
+
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
+
+
+// _________________ version 1 ____________________
+
+
 function pascalCaseSplitter(string) {
     let wordsArrayIndex = [];
 
@@ -18,6 +34,7 @@ function pascalCaseSplitter(string) {
     for (let j = 0; j < wordsArrayIndex.length; j++) {
         wordsArray.push(string.substring(wordsArrayIndex[j], wordsArrayIndex[j + 1]));
     }
+    
     console.log(wordsArray.join(', '));
 }
 
