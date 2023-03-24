7dc39e12f328853c70229b285a0fc18599aaf8e5
theterminal <kirilk777@gmail.com>
2023-03-24 06:47:04 +0200

    Initial

---
+++ 20230223_04_E_JS_Syntax_Fundamentals/100_tests.js
@@ -0,0 +1,13 @@
+function test() {
+    for (let i = 0; i < 10; i++) {
+        if (i === 7) {
+            continue;
+        } else {
+            //
+        }
+
+        console.log(i + ' this is after if else statement');
+    }
+}
+
+test();
