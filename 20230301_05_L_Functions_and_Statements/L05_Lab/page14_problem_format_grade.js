01de1e463d8cf68cfda0a41b1e1b1934299071b4
theterminal <kirilk777@gmail.com>
2023-03-26 08:38:58 +0300

    Refactored

--- 20230301_05_L_Functions_and_Statements/L05_Lab/page14_problem_format_grade.js
+++ 20230301_05_L_Functions_and_Statements/L05_Lab/page14_problem_format_grade.js
@@ -4,15 +4,15 @@
 
 function formatGrade(grade) {
     if (grade < 3) {
-        console.log(`Fail ${grade.toFixed(2)}`);
+        console.log(`Fail (2)}`);                                   // That is how the conditions are given!
     } else if (grade < 3.50) {
-        console.log(`Poor ${grade.toFixed(2)}`);
+        console.log(`Poor ${grade}`);
     } else if (grade < 4.50) {
-        console.log(`Good ${grade.toFixed(2)}`);
+        console.log(`Good ${grade}`);
     } else if (grade < 5.50) {
-        console.log(`Very good ${grade.toFixed(2)}`);
+        console.log(`Very good ${grade}`);
     } else {
-        console.log(`Excellent ${grade.toFixed(2)}`);
+        console.log(`Excellent ${grade}`);
     }
 }
 
