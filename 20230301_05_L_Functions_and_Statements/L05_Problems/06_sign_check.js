091db12f91a9cf97ae79484c9f366f47d962309e
theterminal <kirilk777@gmail.com>
2023-03-29 14:38:19 +0300

    Initial

---
+++ 20230301_05_L_Functions_and_Statements/L05_Problems/06_sign_check.js
@@ -0,0 +1,30 @@
+// 20230301 - JavaScript Front-End 202302 - JS Functions and Statements
+// 06 - Sign Check - judge: https://judge.softuni.org/Contests/Practice/Index/3788#5
+
+
+function signCheck(num1, num2, num3) {
+    let result = '';
+
+    if (num1 < 0 && num2 < 0 && num3 < 0) {
+        result = 'Negative';
+    } else if (num1 < 0 && num2 < 0 && num3 > 0) {
+        result = 'Positive';
+    } else if (num1 < 0 && num2 > 0 && num3 > 0) {
+        result = 'Negative';
+    } else if (num1 > 0 && num2 < 0 && num3 < 0) {
+        result = 'Positive';
+    } else if (num1 > 0 && num2 < 0 && num3 > 0) {
+        result = 'Negative';
+    } else if (num1 > 0 && num2 > 0 && num3 < 0) {
+        result = 'Negative';
+    } else if (num1 > 0 && num2 > 0 && num3 > 0) {
+        result = 'Positive';
+    }
+
+    console.log(result);
+}
+
+signCheck( 5, 12, -15);
+signCheck( -6, -12, 14);
+signCheck( -1, -2, -3);
+signCheck( -5, 1, 1);
