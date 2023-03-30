32452cdf34d9d5fb71ff5ea3232a775a268d5185
theterminal <kirilk777@gmail.com>
2023-03-30 23:51:18 +0300

    Content added

--- 20230301_05_L_Functions_and_Statements/L05_Problems/05_simple_calculator.js
+++ 20230301_05_L_Functions_and_Statements/L05_Problems/05_simple_calculator.js
@@ -2,6 +2,92 @@
 // 05 - Simple Calculator - judge: https://judge.softuni.org/Contests/Practice/Index/3788#4
 
 
+// _______________ version 4 _________________ something else (like dictionary in python)
+
+
+function simpleCalculator4(a, b, operation) {
+    let operations = {
+        'add': a + b,                               // like dictionary with key : value pairs in python
+        'subtract': a - b,
+        'multiply': a * b,
+        'divide': a / b
+    }
+
+    let result = operations[operation];
+    return result;
+}
+
+
+console.log(
+    simpleCalculator4(5, 5, 'multiply'),
+    simpleCalculator4(40, 8, 'divide'),
+    simpleCalculator4(12, 19, 'add'),
+    simpleCalculator4(50, 13, 'subtract')
+);
+
+
+// _______________ version 3 _________________ NOT using switch ____________
+
+
+function simpleCalculator3(num1, num2, operation) {
+    const add = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => a / b;
+    const operationMap = {
+        add : add,                                               // key : value pairs. If they are same, can write only one of the 2
+        subtract : subtract,
+        multiply : multiply,
+        divide : divide
+    }
+
+    return operationMap[operation](num1, num2);                 // operationMap[operation] - returns function, which takes num1 and num2
+}
+
+
+console.log(
+    simpleCalculator3(5, 5, 'multiply'),
+    simpleCalculator3(40, 8, 'divide'),
+    simpleCalculator3(12, 19, 'add'),
+    simpleCalculator3(50, 13, 'subtract')
+);
+
+
+// _______________ version 2 _________________ using arrow (anonymous) functions ____________
+
+
+function simpleCalculator2(num1, num2, operation) {
+    const add = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+    const multiply = (a, b) => a * b;
+    const divide = (a, b) => a / b;
+
+    switch (operation) {
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+        case 'multiply':
+            return multiply(num1, num2);
+        case 'divide':
+            return divide(num1, num2);
+        default:
+            console.log('Something is wrong with the input!');
+    }
+}
+
+
+console.log(
+    simpleCalculator2(5, 5, 'multiply'),
+    simpleCalculator2(40, 8, 'divide'),
+    simpleCalculator2(12, 19, 'add'),
+    simpleCalculator2(50, 13, 'subtract')
+);
+
+
+// _______________ version 1 _________________
+
+
 function simpleCalculator(num1, num2, operator) {
     let result = 0;
 
