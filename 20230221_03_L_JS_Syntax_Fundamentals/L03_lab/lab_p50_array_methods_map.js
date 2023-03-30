dccdbf4fb26a697d6e908b554b5f255e2a2fe21f
theterminal <kirilk777@gmail.com>
2023-03-30 23:03:30 +0300

    Content added

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p50_array_methods_map.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p50_array_methods_map.js
@@ -8,21 +8,35 @@
 // Creates a new array with the results of calling a provided function on every element in the calling array
 
 
-let numbers = [1, 4, 9];
+let numbers1 = [1, 4, 9];
 
-let roots = numbers.map(function(num) {
+let roots = numbers1.map(function(num) {			// num is each element of the array
 	return Math.sqrt(num);
 })
 
 
-console.log(roots);                 // roots is now [1, 2, 3]
-console.log(numbers);               // numbers is still [1, 4, 9]
+console.log(roots);                 			// roots is now [1, 2, 3]
+console.log(numbers1);               			// numbers is still [1, 4, 9]
 
 
-// Another syntax of same method
+// ___________ Another syntax of same method _____ using an arrow function or anonymous function
 
 
 let powers = [2, 4, 5]
 	.map((num) => num ** 2);
 
-console.log(powers);				// [ 4, 16, 25 ]
+console.log(powers);							// [ 4, 16, 25 ]
+
+
+// ___________ Another way of same method _____ if uses a reference to the function
+
+
+let powers3 = [2, 4, 5]
+	.map(multiplyByTwo);						// calls the function 'multiplyByTwo()' and applies it to each element of the array
+
+console.log(powers3);							// [ 4, 16, 25 ]
+
+
+function multiplyByTwo(num) {
+	return num * 2;
+}
