c5008aa011d1615904d07a9698269f6014e1a7c9
theterminal <kirilk777@gmail.com>
2023-03-31 21:07:23 +0300

    Initial

---
+++ 20230302_06_E_Functions_and_Statements/08_perfect_number.js
@@ -0,0 +1,23 @@
+// 20230302 - JavaScript Front-End 202302 - JS Functions and Statements - Exercise
+// 08 - Perfect Number - judge: https://judge.softuni.org/Contests/Compete/Index/3789#7
+
+
+function perfectNumber(number) {
+    let sum = 1;
+
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            sum += i;
+        }
+    }
+
+    if (sum === number) {
+        console.log('We have a perfect number!');
+    } else {
+        console.log("It's not so perfect.");
+    }
+}
+
+perfectNumber(6);
+perfectNumber(28);
+perfectNumber(1236498);
