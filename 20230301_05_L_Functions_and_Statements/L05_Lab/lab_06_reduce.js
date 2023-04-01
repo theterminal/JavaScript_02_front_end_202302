e5436238d469e6273a26a482f89d59b95ea36503
theterminal <kirilk777@gmail.com>
2023-04-01 08:47:58 +0300

    Initial

---
+++ 20230301_05_L_Functions_and_Statements/L05_Lab/lab_06_reduce.js
@@ -0,0 +1,9 @@
+// 20230302 - JavaScript Front-End 202302 - JS Functions and Statements - Exercise
+// Reduce
+
+
+let arrNum = [10, 11, 12, 13, 14];
+
+let sum = arrNum.reduce((x, y) => x + y, 0);
+
+console.log(sum);                                           // 60
