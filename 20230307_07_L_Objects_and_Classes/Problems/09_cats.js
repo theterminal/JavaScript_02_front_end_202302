29c5a4fc0c9387580a6550b3ffa176976b6da646
theterminal <kirilk777@gmail.com>
2023-04-02 10:17:28 +0300

    Initial

---
+++ 20230307_07_L_Objects_and_Classes/Problems/09_cats.js
@@ -0,0 +1,31 @@
+// 20230307 - JavaScript Front-End 202302 - Objects and Classes
+// 09 - Cats - judge: https://judge.softuni.org/Contests/Practice/Index/3791#8
+
+
+function cats(inputArray) {
+    class Cat{
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    let cats = [];
+    for (const line of inputArray) {
+        let [name, age] = line.split(' ');
+        age = Number(age);
+        cats.push(new Cat(name, age));
+    }
+
+    for (const cat of cats) {
+        cat.meow();
+    }
+}
+
+
+cats(['Mellow 2', 'Tom 5']);
+cats(['Candy 1', 'Poppy 3', 'Nyx 2']);
