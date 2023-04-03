afab7ffd9f8c930e0f137388aed9b16e1e0ee444
theterminal <kirilk777@gmail.com>
2023-04-03 07:33:27 +0300

    Refactored

--- 20230309_08_E_Objects_and_Classes/01_employees.js
+++ 20230309_08_E_Objects_and_Classes/01_employees.js
@@ -2,21 +2,23 @@
 // 01 - Employees - judge: https://judge.softuni.org/Contests/Compete/Index/3792#0
 
 
-function employees(nameArray) {
-    let objectEmployees = {};
+// _________________ version 3 ________________ using reduce()
 
-    nameArray.forEach((x) => objectEmployees[x] = x.length);
 
-    let pair = Object.entries(objectEmployees);                                 // object -> array ('pair' is an array)
+function employees3(input) {
+    let objEmployee = input.reduce((data, employee) => {
+        data[employee] = employee.length;
+        return data; 
+    }, {});
 
-    for (const [name, number] of pair) {
-        console.log(`Name: ${name} -- Personal Number: ${number}`);
+    for (const key in objEmployee) {
+        console.log(`Name: ${key} -- Personal Number: ${objEmployee[key]}`);
     }
 }
 
 
-console.log('\n ____________ test 11 ______________ \n')
-employees([
+console.log('\n ____________ test 31 ______________ \n')
+employees3([
     'Silas Butler',
     'Adnaan Buckley',
     'Juan Peterson',
@@ -24,10 +26,77 @@ employees([
     ]);
 
 
-console.log('\n ____________ test 12 ______________ \n')
-employees([
+console.log('\n ____________ test 32 ______________ \n')
+employees3([
     'Samuel Jackson',
     'Will Smith',
     'Bruce Willis',
     'Tom Holland'
     ]);
+
+
+// // _________________ version 2 ________________
+
+
+// function employees2(input) {
+//     let objEmployees = {};
+
+//     for (const employee of input) {
+//         objEmployees[employee] = employee.length;
+//     }
+
+//     for (const key in objEmployees) {
+//         console.log(`Name: ${key} -- Personal Number: ${objEmployees[key]}`);
+//     }
+// }
+
+
+// console.log('\n ____________ test 21 ______________ \n')
+// employees2([
+//     'Silas Butler',
+//     'Adnaan Buckley',
+//     'Juan Peterson',
+//     'Brendan Villarreal'
+//     ]);
+
+
+// console.log('\n ____________ test 22 ______________ \n')
+// employees2([
+//     'Samuel Jackson',
+//     'Will Smith',
+//     'Bruce Willis',
+//     'Tom Holland'
+//     ]);
+
+
+// // _________________ version 1 ________________
+
+
+// function employees(nameStringArray) {
+//     let objectEmployees = {};
+
+//     nameStringArray.forEach((x) => objectEmployees[x] = x.length);
+
+//     let pair = Object.entries(objectEmployees);                                 // object -> array ('pair' is an array)
+//     for (const [name, number] of pair) {
+//         console.log(`Name: ${name} -- Personal Number: ${number}`);
+//     }
+// }
+
+
+// console.log('\n ____________ test 11 ______________ \n')
+// employees([
+//     'Silas Butler',
+//     'Adnaan Buckley',
+//     'Juan Peterson',
+//     'Brendan Villarreal'
+//     ]);
+
+
+// console.log('\n ____________ test 12 ______________ \n')
+// employees([
+//     'Samuel Jackson',
+//     'Will Smith',
+//     'Bruce Willis',
+//     'Tom Holland'
+//     ]);
