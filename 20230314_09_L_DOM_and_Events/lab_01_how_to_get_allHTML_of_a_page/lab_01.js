f3d9048d9672390f05d7be700413435d095566cd
theterminal <kirilk777@gmail.com>
2023-04-05 11:07:09 +0300

    Initial

---
+++ 20230314_09_L_DOM_and_Events/lab_01_how_to_get_allHTML_of_a_page/lab_01.js
@@ -0,0 +1,5 @@
+const htm = document.getElementsByTagName('html')[0];
+console.log(htm.innerHTML);
+// htm.innerHTML = '';                                 // when active - hides the HTML and blank page is displayed
+
+console.log(htm);
