111a8737d4ca7e1b8237dbac492e1580ae585ac4
theterminal <kirilk777@gmail.com>
2023-04-09 16:02:48 +0300

    Content added

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/07_theatre_promotions.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/07_theatre_promotions.js
@@ -2,8 +2,41 @@
 // Problem 07 - Theatre Promotions - judge: https://judge.softuni.org/Contests/Practice/Index/3785#6
 
 
+// ______________ version 2 ______________
+
+
+function theatrePromotion(day, age) {
+    let price;
+    let objDay = {};
+
+    if (0 <= age && age <= 18) {
+        objDay = {'Weekday': 12, 'Weekend': 15, 'Holiday': 5};
+        price = objDay[day];
+    } else if (18 < age && age <= 64) {
+        objDay = {'Weekday': 18, 'Weekend': 20, 'Holiday': 12};
+        price = objDay[day];
+    }  else if (64 < age && age <= 122) {
+        objDay = {'Weekday': 12, 'Weekend': 15, 'Holiday': 10};
+        price = objDay[day];
+    } else {
+        price = 'Error!';
+    }
+
+    console.log(price === 'Error!'?'Error!':`${price}$`);
+}
+
+
+theatrePromotion('Weekday', 42);
+theatrePromotion('Holiday', -12);
+theatrePromotion('Holiday', 15);
+
+
+// ______________ version 1 ______________
+
+
 function theatrePromotions(day, age) {
     let price;
+    
     if (0 <= age && age <= 18) {
         switch (day) {
             case 'Weekday': price = 12; break;
