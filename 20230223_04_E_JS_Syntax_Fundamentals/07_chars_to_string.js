5f68a729cbbc676d64f8edbd6b9eb6a8293783c6
theterminal <kirilk777@gmail.com>
2023-04-10 10:10:04 +0300

    Added content

--- 20230223_04_E_JS_Syntax_Fundamentals/07_chars_to_string.js
+++ 20230223_04_E_JS_Syntax_Fundamentals/07_chars_to_string.js
@@ -2,6 +2,23 @@
 // Problem 07 - Chars to String - judge: https://judge.softuni.org/Contests/Compete/Index/3786#6
 
 
+// _______________ version 2 _________________
+
+
+function charsToString2(...input) {
+    let result = `${input[0]}${input[1]}${input[2]}`;
+    console.log(result);
+}
+
+
+charsToString2('a', 'b', 'c');                               // abc
+charsToString2('%', '2', 'o');                               // %2o
+charsToString2('1', '5', 'p');                               // 15p
+
+
+// _______________ version 1 _________________
+
+
 function charsToString(par_1, par_2, par_3) {
     let result = par_1 + par_2 + par_3;
     console.log(result);
