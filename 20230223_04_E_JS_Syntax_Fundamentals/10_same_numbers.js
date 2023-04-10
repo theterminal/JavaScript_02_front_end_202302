b2e25c5e9ba35a0b48daba1cf21acf159679ee64
theterminal <kirilk777@gmail.com>
2023-04-10 10:10:14 +0300

    Added content

--- 20230223_04_E_JS_Syntax_Fundamentals/10_same_numbers.js
+++ 20230223_04_E_JS_Syntax_Fundamentals/10_same_numbers.js
@@ -2,6 +2,39 @@
 // Problem 10 - Same Numbers - judge: https://judge.softuni.org/Contests/Compete/Index/3786#9
 
 
+// ________________ version 2 _________________
+
+
+function sameNumbers2(input) {
+    let firstDigit = 0;
+    let counter = 0;
+    let sum = 0;
+
+    while (input > 0) {
+        counter++;
+        if (counter == 1) {
+            firstDigit = input % 10;
+        }
+
+        sum += input % 10;
+        input = ~~(input / 10);
+    }
+
+    if (counter * firstDigit === sum) {
+        console.log(`true\n${sum}`);
+        return;
+    }
+    console.log(`false\n${sum}`);
+}
+
+sameNumbers2(2222222);
+sameNumbers2(1234);
+sameNumbers2(0);
+
+
+// ________________ version 1 _________________
+
+
 function sameNumbers(number) {
     number = String(number);
     let sum = 0;
