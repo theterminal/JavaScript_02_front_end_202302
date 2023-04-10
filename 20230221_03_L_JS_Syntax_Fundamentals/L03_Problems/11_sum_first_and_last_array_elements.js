b80157de6e54c1dbbed2dc5aa8b21a5f68f9203e
theterminal <kirilk777@gmail.com>
2023-04-10 10:09:51 +0300

    Added content

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/11_sum_first_and_last_array_elements.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_Problems/11_sum_first_and_last_array_elements.js
@@ -2,6 +2,22 @@
 // Problem 11 - Sum First and Last Array Elements - judge: https://judge.softuni.org/Contests/Practice/Index/3785#10
 
 
+// ________________ version 2 _______________
+
+
+function sumFirstAndLastArrayEl(numbersArr) {
+    console.log(numbersArr.shift() + numbersArr.pop());
+}
+
+
+sumFirstAndLastArrayEl([20, 30, 40]);
+sumFirstAndLastArrayEl([10, 17, 22, 33]);
+sumFirstAndLastArrayEl([11, 58, 69]);
+
+
+// ________________ version 1 _______________
+
+
 function sumFirstAndLastArrayElements(numbersArr) {
     let result;
     let elFirst = numbersArr[0];
