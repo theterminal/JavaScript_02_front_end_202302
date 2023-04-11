702c6797a43d0bc64ae41384b9fe077b56b5bf46
theterminal <kirilk777@gmail.com>
2023-04-11 19:39:16 +0300

    Added content

--- 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p55_strings_searching_for_substrings.js
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/lab_p55_strings_searching_for_substrings.js
@@ -3,19 +3,19 @@
 // Searching for Substrings
 
 
-// ___________ indexOf(substr) ____________
+// ___________ indexOf(substring) ____________
 
 
 let str_1 = "I am JavaScript developer";
 
-console.log(str_1.indexOf("Java"));               // Expected output: 5               
-console.log(str_1.indexOf("java"));               // Expected output: -1
+console.log(str_1.indexOf("Java"));               // Expected output: 5    - returns the index of the first character from the searched string         
+console.log(str_1.indexOf("java"));               // Expected output: -1   - it returns -1 if the result is negative
 
 
-// ___________ lastIndexOf(substr) _____________
+// ___________ lastIndexOf(substring) _____________
 
 
 let str_2 = "Intro to programming";
 let last = str_2.lastIndexOf("o");
 
-console.log(last);                                // Expected output: 11
+console.log(last);                                // Expected output: 11    - returns the last index of the searched string
