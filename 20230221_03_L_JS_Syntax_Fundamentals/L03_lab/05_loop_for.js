862289ca3e3d5f39bc925f0a5f9750b6fde28bea
theterminal <kirilk777@gmail.com>
2023-04-11 13:51:58 +0300

    Initial

---
+++ 20230221_03_L_JS_Syntax_Fundamentals/L03_lab/05_loop_for.js
@@ -0,0 +1,11 @@
+// JavaScript
+// 'for' loop - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#for_statement
+
+
+// The for statement creates a loop that consists of three optional expressions, enclosed in parentheses and separated by semicolons, followed by a statement (usually a block statement) to be executed in the loop.
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
