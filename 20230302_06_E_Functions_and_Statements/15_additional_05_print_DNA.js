1b02bd2c61395b9c670f3871c6af1ff34cd2f815
theterminal <kirilk777@gmail.com>
2023-04-14 09:09:35 +0300

    Initial

---
+++ 20230302_06_E_Functions_and_Statements/15_additional_05_print_DNA.js
@@ -0,0 +1,40 @@
+// 20230303 - JavaScript Front-End 202302 - JS Functions and Statements - More Exercise
+// More Exercises 05 - Print DNA - judge: https://judge.softuni.org/Contests/Practice/Index/3790#4
+
+
+function printDNA(input) {
+    const arrElements = ['AT', 'CG', 'TT', 'AG', 'GG'];
+    let result = '';
+
+    for (let i = 0; i < input; i++) {
+        let index = returnPattern(i).indexOf('K');
+        
+        if (i < 5) {
+            currentEl = i;
+        } else {
+            currentEl = i % 5;
+        }
+
+        if (index === 0) {
+            result += `${arrElements[currentEl][0]}----${arrElements[currentEl][1]}\n`;
+        } else if (index === 1) {
+            result += `*${arrElements[currentEl][0]}--${arrElements[currentEl][1]}*\n`;
+        } else if (index === 2) {
+            result += `**${arrElements[currentEl][0]}${arrElements[currentEl][1]}**\n`;
+        }
+    }
+    console.log(result);
+
+    function returnPattern(number) {
+        const objPattern = {0: '**KK**', 1: '*K--K*', 2: 'K----K', 3: '*K--K*'};
+        if (number > 3) {
+            return objPattern[number % 4];
+        } else {
+            return objPattern[number];
+        }
+    }
+}
+
+
+// printDNA(4);
+printDNA(10);
