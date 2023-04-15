cf3a5491e844a3f818c438375e9346163f1d263b
theterminal <kirilk777@gmail.com>
2023-04-15 09:26:41 +0300

    Renamed file and added more content

---
+++ 20230307_07_L_Objects_and_Classes/Lab/02_forin_loop_used_for_objects_and_arrays.js
@@ -0,0 +1,31 @@
+// 20230307 - JavaScript Front-End 202302 - Objects and Classes
+// forin loop - good for objects and arrays 
+
+
+// Used mainly for OBJECTS.
+// When used with object gives the key. See example 02.
+// When used with arrays gives the index. See example 03.
+
+
+console.log('\n __________ ex. forin 01 __________ object __________  \n');
+
+
+let cat = {
+    name: 'Tom',
+    age: 12,
+    color: 'blue'
+};
+
+for (const key in cat) {                                                    // 'key' is the key in the object and is used to get the 'value'
+    console.log(key + ' : ' + cat[key]);
+}
+
+
+console.log('\n ___________ ex. forin 02 __________array __________ used to get index and then the element at that index in an array __________ \n');
+
+
+let dog = [12, 23, 23, 23];
+
+for (i in dog) {                                                            // 'i' is the index and is used to get the element in the array
+    console.log(i, dog[i]);
+}
