4cadaa8f120e66bc650342785daf8aae9e8683d0
theterminal <kirilk777@gmail.com>
2023-04-16 16:33:12 +0300

    Added more content and explanations

--- 20230307_07_L_Objects_and_Classes/Lab/lab_page_30_JSON.js
+++ 20230307_07_L_Objects_and_Classes/Lab/lab_page_30_JSON.js
@@ -4,3 +4,38 @@
 
 // Cannot have functions in JSON format
 
+
+// _______________ converting Object to JSON String _____________
+
+
+function convertObjectToJSON(firstName, lastName, hairColor) {
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        hairColor: hairColor
+    }
+
+    let jsonPerson = JSON.stringify(person);
+
+    console.log(person);                                        // object 'person' was created from the input information
+    console.log(jsonPerson);                                    // this JSON string was created from the object 'person'
+}
+
+
+convertObjectToJSON('George', 'Jones', 'Brown');
+
+
+// _______________ converting JSON string to Object _______________
+
+
+function convertJSONStringToObject(json) {
+    let object = JSON.parse(json);
+    let entires = Object.entries(object);
+
+    for (const [key, value] of entires) {
+        console.log(`${key}: ${value}`);
+    }
+}
+
+
+convertJSONStringToObject('{"name": "George", "age": 40, "town": "Sofia"}');
