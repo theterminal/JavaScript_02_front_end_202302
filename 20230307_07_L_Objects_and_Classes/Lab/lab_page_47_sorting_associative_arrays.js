26483883e1db747fc832e43e4e5f6f81d28626d1
theterminal <kirilk777@gmail.com>
2023-04-16 16:49:53 +0300

    Editing the examples and adding more content

--- 20230307_07_L_Objects_and_Classes/Lab/lab_page_47_sorting_associative_arrays.js
+++ 20230307_07_L_Objects_and_Classes/Lab/lab_page_47_sorting_associative_arrays.js
@@ -2,47 +2,60 @@
 // Page 47 - Sorting Associative Arrays 
 
 
-// __________________ sorting by email ________________________________
+console.log('\n __________________ sorting by ASCENDING by email ________________________________ \n');
 
 
-let people = {
+let people1 = {
     'Kiril': {age: 25, email: 'kiril@gmail.com'},
     'Peter': {age: 29, email: 'peter@gmail.com'},
     'George': {age: 45, email: 'george@gmail.com'}
 };
 
-let entries = Object.entries(people);
+let entries1 = Object.entries(people1);
 
-let sortedByName = entries.sort((personA, personB) => {
-    let personAEmail = personA[1].email;
-    let personBEmail = personB[1].email;
-    return personAEmail.localeCompare(personBEmail);
-})
+let sortedByName1 = entries1
+    .sort((personA, personB) => {
+        let personAEmail = personA[1].email;
+        let personBEmail = personB[1].email;
+        
+        return personAEmail.localeCompare(personBEmail);
+    })
 
-for (const [name, info] of sortedByName) {
+for (const [name, info] of sortedByName1) {
     console.log(name);
     console.log(info);
 }
 
 
-// __________________ sorting by name ________________________________
+console.log('\n __________________ sorting DESCENDING by name ___________________________________ \n');
 
 
-// let people = {
-//     'Kiril': {age: 25, email: 'kiril@gmail.com'},
-//     'Peter': {age: 29, email: 'peter@gmail.com'},
-//     'George': {age: 45, email: 'george@gmail.com'}
-// };
+let people2 = {
+    'Kiril': {age: 25, email: 'kiril@gmail.com'},
+    'Peter': {age: 29, email: 'peter@gmail.com'},
+    'George': {age: 45, email: 'george@gmail.com'}
+};
 
-// let entries = Object.entries(people);
+let entries2 = Object.entries(people2);
 
-// let sortedByName = entries.sort((personA, personB) => {
-//     let personAName = personA[0];
-//     let personBName = personB[0];
-//     return personAName.localeCompare(personBName);
-// })
+let sortedByName2 = entries2
+    .sort((personA, personB) => {
+        let personAName = personA[0];
+        let personBName = personB[0];
+        return personBName.localeCompare(personAName);
+    })
 
-// for (const [name, info] of sortedByName) {
-//     console.log(name);
-//     console.log(info);
-// }
+for (const [name, info] of sortedByName2) {
+    console.log(name);
+    console.log(info);
+}
+
+
+console.log('\n _______________ iterating trough the Associative Array using \'for in\' loop _______________ \n');
+
+
+let assArr = {'name': 'John', 'color': 'blue'};                 // 'assArr' - example of Associative Array
+
+for (const key in assArr) {                                     // iterating through the keys in 'assArr'
+     console.log(key, assArr[key]);                             // printing the 'key value' pairs of the Associative Array 'assArr'
+    }
