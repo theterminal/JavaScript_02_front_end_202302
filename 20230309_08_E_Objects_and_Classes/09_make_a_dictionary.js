c0785da75bf4e79f77705c2e778ddcd119ddf6f3
theterminal <kirilk777@gmail.com>
2023-04-17 22:32:10 +0300

    Added more test data

--- 20230309_08_E_Objects_and_Classes/09_make_a_dictionary.js
+++ 20230309_08_E_Objects_and_Classes/09_make_a_dictionary.js
@@ -21,6 +21,7 @@ function dictionaryFromJSON(inputJSON) {
 }
 
 
+console.log('\n _______________ test 11 _______________ \n');
 dictionaryFromJSON([
     '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
     '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
@@ -28,3 +29,13 @@ dictionaryFromJSON([
     '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
     '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
 ]);
+
+
+console.log('\n _______________ test 12 _______________ \n');
+dictionaryFromJSON([
+    '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+    '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+    '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+    '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+    '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} '
+])
