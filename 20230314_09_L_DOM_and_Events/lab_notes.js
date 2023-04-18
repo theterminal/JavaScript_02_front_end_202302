7b54297b7953d1dd81359663ca8239ec03ad66be
theterminal <kirilk777@gmail.com>
2023-04-18 16:19:47 +0300

    Initial

---
+++ 20230314_09_L_DOM_and_Events/lab_notes.js
@@ -0,0 +1,24 @@
+// 20230314 - 09 - L - DOM and Events
+
+
+// .getElementsByTagName() returns an array
+
+// using 'style' in JS on elements we use inline styles they will override the existing css styles
+
+// When using NODE LIST (querySelector and querySelectorAll), always transform into an array. Then all the array methods can be called. Same actually applies to the HTML Collection (getElementsByTagName, getElementsById). The HTML Collection and NODE List do NOT have those same methods.
+
+// 'Array.form(nameOfCollectionToBeConverted)' is identical as [...nameOfCollectionToBeConverted]
+
+// _______ methods _______
+
+/*
+.getAttribute()
+.setAttribute()
+.document.getElementsByTagName()
+
+textContent (all text between opening and closing tags)
+innerHTML
+value (input elements for example, forms...)
+style 
+*/
+
