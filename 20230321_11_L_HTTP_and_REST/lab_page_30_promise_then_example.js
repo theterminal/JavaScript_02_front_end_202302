5312abf53878164eb1eabab9a25336282dee5178
theterminal <kirilk777@gmail.com>
2023-04-19 08:09:23 +0300

    Initial

---
+++ 20230321_11_L_HTTP_and_REST/lab_page_30_promise_then_example.js
@@ -0,0 +1,12 @@
+console.log('Before promise');
+
+new Promise(function (resolve, reject) {
+    setTimeout(function() {
+        resolve('done');
+    }, 1500);
+})
+.then(function(res) {
+    console.log('Then returned: ' + res);
+});
+
+console.log('After promise');
