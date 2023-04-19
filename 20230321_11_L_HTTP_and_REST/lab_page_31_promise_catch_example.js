4be7143ae5cca15e0415c34e3da8ceb8869d5f9c
theterminal <kirilk777@gmail.com>
2023-04-19 15:59:12 +0300

    Added some content

--- 20230321_11_L_HTTP_and_REST/lab_page_31_promise_catch_example.js
+++ 20230321_11_L_HTTP_and_REST/lab_page_31_promise_catch_example.js
@@ -13,3 +13,22 @@ new Promise(function (resolve, reject) {
 .catch(function(error) {console.log(error); });
 
 console.log('After promise');
+
+
+
+// Popular Promise Methods
+// =======================
+
+// Promise.reject(reason)
+//     Returns an object that is rejected with the given reason
+
+// Promise.resolve(value)
+//     Returns an object that is resolved with the given value
+
+// Promise.finally()
+//     The handler is called when the promise is settled
+
+// Promise.all(iterable)
+//     Returns a promise 
+//         Fulfills when all of the promises have fulfilled 
+//         Rejects as soon as one of them rejects
