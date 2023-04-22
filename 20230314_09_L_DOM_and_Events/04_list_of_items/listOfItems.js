ea0efb791f76458f8e6d7f7550984b83659c70da
theterminal <kirilk777@gmail.com>
2023-04-22 15:04:59 +0300

    Refactored

--- 20230314_09_L_DOM_and_Events/04_list_of_items/listOfItems.js
+++ 20230314_09_L_DOM_and_Events/04_list_of_items/listOfItems.js
@@ -1,6 +1,10 @@
+// 20230314 - JavaScript Front-End - DOM and Events - Lab
+// 04 - List of Items - judge: https://judge.softuni.org/Contests/Practice/Index/3794#3
+
+
 function addItem() {
-    const ulElement = document.getElementById('items');
     const inputIn = document.getElementById('newItemText');
+    const ulElement = document.getElementById('items');
     const newLi = document.createElement('li');
 
     newLi.textContent = inputIn.value;
