45969f4241eb425a71c40ae1a77021e47e5e9878
theterminal <kirilk777@gmail.com>
2023-04-22 14:55:17 +0300

    Added a link at top of page

--- 20230314_09_L_DOM_and_Events/02_show_more/ShowMore.js
+++ 20230314_09_L_DOM_and_Events/02_show_more/ShowMore.js
@@ -1,3 +1,7 @@
+// 20230314 - JavaScript Front-End - DOM and Events - Lab
+// 02 - Show More - judge: https://judge.softuni.org/Contests/Practice/Index/3794#1
+
+
 function showText() {
     const anchor = document.getElementById('more');
     const span = document.getElementById('text');
