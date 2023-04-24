b728ddb80b294e20027f3ae14c887d3038528913
theterminal <kirilk777@gmail.com>
2023-04-24 07:23:26 +0300

    Added more content

--- 20230314_09_L_DOM_and_Events/07_colorize_table/colorizeTable.js
+++ 20230314_09_L_DOM_and_Events/07_colorize_table/colorizeTable.js
@@ -1,9 +1,17 @@
+// 20230314 - JavaScript Front-End - DOM and Events - Lab
+// 07 - Colorize Table - judge: https://judge.softuni.org/Contests/Practice/Index/3794#6
+
+
 function colorize() {
     const rows = document.querySelectorAll('tr');
 
     for (let i = 0; i < rows.length; i++) {
         if (i % 2 === 1) {
-            rows[i].setAttribute("style","background-color: teal");
+            // option 1 (choose option 1 or option 2 and comment out the other)
+            // rows[i].setAttribute("style","background-color: teal");
+
+            // option 2 (choose option 1 or option 2 and comment out the other)
+            rows[i].style.backgroundColor = 'teal';
         }
     }
 }
