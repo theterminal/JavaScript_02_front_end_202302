d840e408f90c3c85cee33c454036e3bf82a1eb9e
theterminal <kirilk777@gmail.com>
2023-04-24 07:58:50 +0300

    Added some content

--- 20230314_09_L_DOM_and_Events/lab_01_how_to_get_allHTML_of_a_page/lab_01.js
+++ 20230314_09_L_DOM_and_Events/lab_01_how_to_get_allHTML_of_a_page/lab_01.js
@@ -1,5 +1,11 @@
+// It'll grab all of the HTML on the page and will convert it to an Array of elements
 const htm = document.getElementsByTagName('html')[0];
-console.log(htm.innerHTML);
-// htm.innerHTML = '';                                 // when active - hides the HTML and blank page is displayed
 
+// It takes the Head and Body elements and their content
+console.log(htm.innerHTML);
+
+// when next line is active - it'll hide the HTML and a blank page will be displayed
+// htm.innerHTML = '';                                 
+
+// It takes the full page
 console.log(htm);
