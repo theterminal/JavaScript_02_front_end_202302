653e83ac4b68cb5b3a9b79df6d9a2aba87db507e
theterminal <kirilk777@gmail.com>
2023-04-24 07:42:55 +0300

    Refactored

--- 20230314_09_L_DOM_and_Events/08_sum_table/sumTable.js
+++ 20230314_09_L_DOM_and_Events/08_sum_table/sumTable.js
@@ -1,32 +1,17 @@
+// 20230314 - JavaScript Front-End - DOM and Events - Lab
+// 08 - Sum Table - judge: https://judge.softuni.org/Contests/Practice/Index/3794#7
+
+
 function sumTable() {
     let table = document.getElementsByTagName('table')[0];
+    let rows = table.rows;
     let sum = 0;
 
-    for (let i = 0; i < table.rows.length; i++) {
-        let row = table.rows[i];
-        
-        if (i > 0 && i < table.rows.length - 1) {
-            let price = Number(row.cells[1].innerHTML);
-            sum += price;
-        }
+    for (let i = 1; i < table.rows.length - 1; i++) {
+        let price = Number(rows[i].cells[1].innerHTML);
+        sum += price;
     }
 
     const totalField = document.getElementById('sum');
     totalField.textContent = sum.toFixed(2);
 }
-
-
-// ______ CGPT4 answer _______
-
-
-// function sumTable() {
-//     var table = document.getElementsByTagName('table')[0];
-//     var rows = table.rows;
-//     var sum = 0;
-    
-//     for (var i = 1; i < rows.length - 1; i++) {
-//       sum += parseFloat(rows[i].cells[1].textContent);
-//     }
-    
-//     document.getElementById('sum').textContent = sum.toFixed(2);
-//   }
